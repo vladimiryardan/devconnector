97356a07ea8c58a7b9d14210054f43b3b2b72608
Vladimir Yardan <>
2020-06-26 13:15:04 +0800

    edit profile and delete account

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -4,7 +4,8 @@ import {
   GET_PROFILE,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
-  GET_ERRORS
+  GET_ERRORS,
+  SET_CURRENT_USER
 } from './types';
 
 // Get current profile
@@ -45,6 +46,23 @@ export const setProfileLoading = () => {
     type: PROFILE_LOADING
   };
 };
+
+//Delete Account and Profile
+export const deleteAccount = () => dispatch => {
+  if(window.confirm('Are you sure? This can NOT be undone'));
+  axios
+    .delete('/api/profile')
+    .then(res =>
+      dispatch({
+        type: SET_CURRENT_USER,
+        payload: {}
+      })
+    ).catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      }))
+}
 
 // Clear profile
 export const clearCurrentProfile = () => {
