4b52ea33ad7e9275e1762c6652115620bf439323
Vladimir Yardan <>
2020-06-26 10:47:57 +0800

    create profile, actions, include in app

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
 
 import './App.css';
 import { clearCurrentProfile } from './actions/profileActions';
@@ -59,6 +60,12 @@ class App extends Component {
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
+              <Switch>
+                <PrivateRoute 
+                  exact 
+                  path="/create-profile" 
+                  component={CreateProfile} />
+              </Switch>              
             </div>    
           <Footer />
           </div>
