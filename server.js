f7d4870cd11ed7562be82211095f760ce913b5bb
Vladimir Yardan <>
2020-08-11 00:29:01 +0800

    prepare to deploy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
 const posts = require('./routes/api/posts')
-
+const path = require('path');
 
 
 const app = express();
@@ -32,6 +32,17 @@ require('./config/passport')(passport);
 app.use('/api/users',users);
 app.use('/api/profile',profile);
 app.use('/api/posts',posts);
+
+//server static assets if in production
+if(process.env.NODE_ENV = 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+    
+    app.get('*',(req,res) =>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
+
 
 const port = process.env.port || 5000;
 
